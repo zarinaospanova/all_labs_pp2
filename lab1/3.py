09469f058572b906f4a9a6db1ecf497ad0194fbb
Zarina Ospanova <darinaospanova@Noutbuk-darina.Dlink>
2025-01-21 23:42:39 +0500

    update lab1

---
+++ lab1/3.py
@@ -0,0 +1,53 @@
+# variables w3school
+
+a = 10
+b = "Kate"
+print(a)
+print(b)
+
+
+# casting 
+x = str(10) # x will be '10'  - string
+y = int(10) #y will be 10  - integer
+z = float(10) #z will be 10.0  - float
+
+# get the type 
+q = 5
+w = "Anna"
+print(type(q))
+print(type(w))
+
+#Single or Double Quotes?
+e = "Kim"
+r = 'Kim'  # they are same as 
+
+#Variable Names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Many values to Multiple Variables
+i,o,p = "LA","New York","Boston"
+print(i)
+print(o)
+print(p)
+
+# One Value to Multiple Variables
+v = f = g = "Harvard"
+print(v)
+print(f)
+print(g)
+
+# Global Variables
+t = "MIT"
+
+def myFunc():
+    print("The best university in the world is" + t)
+myFunc()
+
+
+
+

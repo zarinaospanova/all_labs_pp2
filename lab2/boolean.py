7ae339e9208e5a86db41ce40f9aa4dd57bb57c64
Zarina Ospanova <darinaospanova@Noutbuk-darina.Dlink>
2025-01-22 23:30:25 +0500

    lab2 update

---
+++ lab2/boolean.py
@@ -0,0 +1,30 @@
+print(10>9) #true
+print(10 == 9) #false
+print(10<9) #false
+
+a = 300
+b = 44
+
+if b>a:
+    print("b greater than a")
+else:
+    print("b is not greater than a")
+
+print(bool("hi"))
+print(bool(12))
+
+x = "KBTU"
+y = 13
+
+print(bool(x))
+print(bool(y))
+
+bool('abc')
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
---
+++ lab2/operators.py
@@ -0,0 +1,28 @@
+print(12+8)
+
+'''
++ addition
+- subtaction
+* multiplication
+/ division
+% modulus
+** exponentation
+// floor division
+
+
+python comprasion operators
+
+== equal
+!= not equal
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+
+
+python logical operators
+and conj &
+or disj |
+not nig ~
+xor ^
+'''
